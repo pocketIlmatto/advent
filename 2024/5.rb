4a6a62d3ca2f3110d28cd8033a3a4856c189b3b3
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2024-12-11 16:51:49 -0800

    Finished 5a

--- 2024/5.rb
+++ 2024/5.rb
@@ -14,19 +14,16 @@ def build_rules_hash(rules_array, test_mode)
 
 
   rules_array.each do |rule|
-    # puts "Processing rule: #{rule}" if test_mode
+    puts "Processing rule: #{rule}" if test_mode
     a, b = rule.split('|')
 
     rules[a] ||= {before: [], after: []}
     rules[a][:before] << b
-    # puts "Rules for #{a}: #{rules[a]}" if test_mode
 
 
     rules[b] ||= {before: [], after: []}
     rules[b][:after] << a
-    # puts "Rules for #{b}: #{rules[b]}" if test_mode
   end
-  # puts rules.inspect if test_mode
   rules
 end
 
@@ -34,13 +31,34 @@ def check_updates(updates, rules_hash, test_mode)
   middle_page_sum = 0
 
   updates.each do |update|
-    (0..update.length-1).each do |i|
-      pages_before = update[0..i]
-      pages_after = update[i+1..update.length-1]
+    update_pages = update.split(',')
+    is_update_legal = true
 
 
+    (0..update_pages.length-1).each do |i|
+      current_page = update_pages[i]
+      pages_before = update_pages[0..i]
+      pages_after = update_pages[i+1..update_pages.length-1]
+
+      illegal_before_pages = pages_before.intersection(rules_hash[current_page][:before])
+      if illegal_before_pages.length > 0
+        puts "Update #{update_pages}, page #{current_page} breaks a rule: #{illegal_before_pages} should not come before #{current_page}" if test_mode
+        is_update_legal = false
+      end
+
+      illegal_after_pages = pages_after.intersection(rules_hash[current_page][:after])
+      if illegal_after_pages.length > 0
+        puts "Update #{update_pages}, page #{current_page} breaks a rule: #{illegal_after_pages} should not come after #{current_page}" if test_mode
+        is_update_legal = false
+      end
     end
 
+    if is_update_legal
+      middle_page = update_pages[(update_pages.length-1)/2].to_i
+      puts "Adding middle page (#{middle_page} of this update: #{update_pages}" if test_mode
+      middle_page_sum += middle_page
+    end
+    is_update_legal = true
   end
 
   middle_page_sum
@@ -48,8 +66,8 @@ end
 
 def five_a(input_file, test_mode = true)
   result = split_file_sections(input_file)
-  build_rules_hash(result[:top_section], test_mode)
-  puts check_updates(result[:bottom_section], test_mode)
+  rules_hash = build_rules_hash(result[:top_section], test_mode)
+  puts check_updates(result[:bottom_section], rules_hash, test_mode)
 end
 
-five_a('5testInput')
+five_a('5input', true)
